92c1a6d4745580116ed756964e5d7c39e28ff7a8
Jan-Willem Taylor Parkins <0891769@hr.nl>
2017-09-25 09:30:53 +0200

    push

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse">
+    <div class='container'>
+        <div class='navbar-header'>
+            {{--  <button types='button' class='nabar-toggle collapsed' data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>  --}}
+            <a class="navbar-brand" href="http://localhost/Jaar2/laravel/test_Laravel1/public/">Laravel Project</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class=" nav navbar-nav">
+                <li><a href="http://localhost/Jaar2/laravel/test_Laravel1/public/">Home</a></li>
+                <li><a href="http://localhost/Jaar2/laravel/test_Laravel1/public/about">About</a></li>
+                <li><a href="http://localhost/Jaar2/laravel/test_Laravel1/public/services">Services</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
